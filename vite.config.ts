a0ed14f90b8cd7e14518aa85b6bd9c8520cc8326
Harshita Rajput <harshur193@gmail.com>
2025-12-10 16:53:24 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import { metaImagesPlugin } from "./vite-plugin-meta-images";
 
 export default defineConfig({
+  // IMPORTANT for GitHub Pages
+  base: "/project-flip/",
+
   plugins: [
     react(),
     runtimeErrorOverlay(),
@@ -21,12 +24,6 @@ export default defineConfig({
     },
   },
 
-  css: {
-    postcss: {
-      plugins: [],
-    },
-  },
-
   root: path.resolve(import.meta.dirname, "client"),
 
   build: {
@@ -34,12 +31,9 @@ export default defineConfig({
     emptyOutDir: true,
   },
 
-  server: {
-    host: "0.0.0.0",
-    allowedHosts: true,
-    fs: {
-      strict: true,
-      deny: ["**/.*"],
+  css: {
+    postcss: {
+      plugins: [],
     },
   },
 });
